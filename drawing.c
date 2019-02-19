88f7e21fa82b6b13a2b5abc11cb8c5a630630e1e
mareksuma1985 <msuma@wp.pl>
2019-02-19 22:12:00 +0100

    artificial horizon now has colors

--- drawing.c
+++ drawing.c
@@ -1,18 +1,50 @@
 #include <cairo.h>
-#include <gdk/gdk.h>
+#include <gtk/gtk.h>
+#include <math.h>
 
-gboolean draw_callback(GtkWidget *widget, cairo_t *cr, gpointer data) {
-	guint width, height;
-	GdkRGBA color;
+double roll_rads;
+int pitch_pixels;
 
-	width = gtk_widget_get_allocated_width(widget);
-	height = gtk_widget_get_allocated_height(widget);
-	cairo_arc(cr, width / 2.0, height / 2.0, MIN(width, height) / 2.0, 0, 2 * G_PI);
+static void draw_ground(cairo_t *cr, GtkWidget *widget)
+{
+int width, height;
+width = gtk_widget_get_allocated_width(widget);
+height = gtk_widget_get_allocated_height(widget);
 
-	gtk_style_context_get_color(gtk_widget_get_style_context(widget), 0, &color);
-	gdk_cairo_set_source_rgba(cr, &color);
+/* paint background blue */
+ cairo_set_source_rgb(cr, 0.16, 0.47, 0.94);
+ cairo_paint (cr);
+  
+/* draw brown ground */  
+  cairo_set_source_rgb(cr, 0.54, 0.28, 0);
+  cairo_set_line_width(cr, 1);
 
-	cairo_fill(cr);
+  cairo_move_to(cr, 0, height/2 + pitch_pixels - roll_rads * 90); // upper left verticle
+  cairo_line_to(cr, width, height/2 + pitch_pixels + roll_rads * 90); // upper right verticle
+  cairo_line_to(cr, width, height); // lower right corner
+  cairo_line_to(cr, 0, height); // lower left corner
+  cairo_close_path(cr);
+  cairo_stroke_preserve(cr);
+  cairo_fill(cr);
+}
 
-	return FALSE;
+static void draw_symbol(cairo_t *cr, GtkWidget *widget)
+{
+int width, height;
+width = gtk_widget_get_allocated_width(widget);
+height = gtk_widget_get_allocated_height(widget);
+
+/* draw aircaft attitude symbol */
+cairo_set_source_rgb (cr, 1, 1, 1);
+cairo_move_to (cr, width/8, height/2);
+cairo_line_to (cr, 7*width/8, height/2);
+cairo_set_line_width (cr, 4);
+cairo_stroke (cr);
+}
+
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data)
+{
+  draw_ground(cr, widget);
+  draw_symbol(cr, widget);
+  return FALSE;
 }
