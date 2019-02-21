665f87e408b07d3236abd1a5255d996bb1bc1886
mareksuma1985 <msuma@wp.pl>
2019-02-21 16:08:43 +0100

    updated to gstreamer-1.0 and gtk+-3.0
    gstreamer-0.10 to gstreamer-1.0
    gtk+-2.0 to gtk+-3.0

--- make_client.sh
+++ make_client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 touch ./telerobot_client.c
-gcc `pkg-config --cflags gtk+-2.0 gstreamer-0.10 gstreamer-interfaces-0.10` telerobot_client.c -o telerobot_client `pkg-config --libs gtk+-2.0 gstreamer-0.10 gstreamer-interfaces-0.10` -lm
+gcc `pkg-config --cflags gtk+-3.0 gstreamer-1.0 cairo` telerobot_client.c -o client `pkg-config --libs gtk+-3.0 gstreamer-1.0 gstreamer-video-1.0 gstreamer-base-1.0 cairo` -I "/usr/include/cairo" -I "/usr/include/gtk-3.0" -I "/usr/include/gstreamer-1.0" -lm -w
 read x
