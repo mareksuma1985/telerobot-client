f66bd9a910460c475724beeee77067a702e6fcd8
mareksuma1985 <msuma@wp.pl>
2019-02-21 16:06:40 +0100

    artificial horizon now has colors

--- drawing.c
+++ drawing.c
@@ -2,49 +2,48 @@
 #include <gtk/gtk.h>
 #include <math.h>
 
+float sensor_x = 0;
+float sensor_y = 0;
+float sensor_z = 10;
+
 double roll_rads;
-int pitch_pixels;
+double pitch_rads;
 
-static void draw_ground(cairo_t *cr, GtkWidget *widget)
-{
-int width, height;
-width = gtk_widget_get_allocated_width(widget);
-height = gtk_widget_get_allocated_height(widget);
+static void draw_ground(cairo_t *cr, GtkWidget *widget) {
+	int width = gtk_widget_get_allocated_width(widget);
+	int height = gtk_widget_get_allocated_height(widget);
 
-/* paint background blue */
- cairo_set_source_rgb(cr, 0.16, 0.47, 0.94);
- cairo_paint (cr);
-  
-/* draw brown ground */  
-  cairo_set_source_rgb(cr, 0.54, 0.28, 0);
-  cairo_set_line_width(cr, 1);
+	/* paint background blue */
+	cairo_set_source_rgb(cr, 0.16, 0.466, 0.937);
+	cairo_paint(cr);
 
-  cairo_move_to(cr, 0, height/2 + pitch_pixels - roll_rads * 90); // upper left verticle
-  cairo_line_to(cr, width, height/2 + pitch_pixels + roll_rads * 90); // upper right verticle
-  cairo_line_to(cr, width, height); // lower right corner
-  cairo_line_to(cr, 0, height); // lower left corner
-  cairo_close_path(cr);
-  cairo_stroke_preserve(cr);
-  cairo_fill(cr);
+	/* draw ground */
+	cairo_set_source_rgb(cr, 0.537, 0.278, 0);
+	cairo_set_line_width(cr, 1);
+
+	cairo_move_to(cr, 0, height / 2 + (sensor_y / sensor_z) * width / 2 - (sensor_x / sensor_z) * width / 2);
+	cairo_line_to(cr, width, height / 2 + (sensor_y / sensor_z) * width / 2 + (sensor_x / sensor_z) * width / 2);
+	cairo_line_to(cr, width, height);
+	cairo_line_to(cr, 0, height);
+	cairo_close_path(cr);
+	cairo_stroke_preserve(cr);
+	cairo_fill(cr);
 }
 
-static void draw_symbol(cairo_t *cr, GtkWidget *widget)
-{
-int width, height;
-width = gtk_widget_get_allocated_width(widget);
-height = gtk_widget_get_allocated_height(widget);
+static void draw_nivel(cairo_t *cr, GtkWidget *widget) {
+	int width = gtk_widget_get_allocated_width(widget);
+	int height = gtk_widget_get_allocated_height(widget);
 
-/* draw aircaft attitude symbol */
-cairo_set_source_rgb (cr, 1, 1, 1);
-cairo_move_to (cr, width/8, height/2);
-cairo_line_to (cr, 7*width/8, height/2);
-cairo_set_line_width (cr, 4);
-cairo_stroke (cr);
+	/* draw aircraft attitude symbol */
+	cairo_set_source_rgb(cr, 1, 1, 1);
+	cairo_move_to(cr, width / 8, height / 2);
+	cairo_line_to(cr, 7 * width / 8, height / 2);
+	cairo_set_line_width(cr, 4);
+	cairo_stroke(cr);
 }
 
-static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data)
-{
-  draw_ground(cr, widget);
-  draw_symbol(cr, widget);
-  return FALSE;
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
+	draw_ground(cr, widget);
+	draw_nivel(cr, widget);
+	return FALSE;
 }
